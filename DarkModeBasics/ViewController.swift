92bc0439afae376e85c1f5d82c335bd3c9fd376b
Alican Kurt <kurta15@itu.edu.tr>
2021-08-13 14:50:52 +0300

    Project Commit

--- DarkModeBasics/ViewController.swift
+++ DarkModeBasics/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        // if we want to work just this style ( Just this controller )
+        overrideUserInterfaceStyle = .light
+        // if we want to work just one style whole app, we must change in the file info.plist
+        
+        
     }
 
+    // it works when any trait Collection changed || if user Interface  darkmode
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        // Get current interface style
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark{
+            //xxxxx
+        }else{
+            //xxxxx
+        }
+        
+        
+    }
 
 }
 
